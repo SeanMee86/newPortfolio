7861901d2ff91c95e1acde6af2711410296d13d7
SeanMee86 <seanmee8186@gmail.com>
2020-02-24 21:36:11 -0800

    added typewriter effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,33 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
+import indexStyles from './index.module.scss'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
 
+
+    let [text, setText] = useState('');
+    let [counter, setCounter] = useState(0);
+
+    const textArray = 'My name is Sean Mee I am a Full Stack Web Developer Living in Southern California Who Specializes in JavaScript'.split('');
+
+
+    useEffect(() => {
+        const addChar = setInterval(() => {
+            setText(text = text + textArray[counter]);
+            setCounter(counter = counter+1);
+            if(counter >= textArray.length){
+                clearInterval(addChar);
+            }
+        }, 50);
+    }, []);
+
     return(
         <Layout>
             <SEO title="Home" />
-            <div>
-                <h1>Home</h1>
+            <div className={indexStyles.container}>
+                <p>{text}</p>
             </div>
         </Layout>
     )
