5f0a28bd0bcb0534d06d891c2f67ceadf01474a9
hss-smee <seanmee8186@gmail.com>
2020-02-26 09:52:30 -0800

    implementing useRef

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState, useRef} from "react"
 import indexStyles from './index.module.scss'
 
 import Layout from "../components/layout"
@@ -6,28 +6,37 @@ import SEO from "../components/seo"
 
 const IndexPage = () => {
 
-
-    let [text, setText] = useState('');
-    let [counter, setCounter] = useState(0);
-
     const textArray = 'My name is Sean Mee I am a Full Stack Web Developer Living in Southern California Specializing in JavaScript'.split('');
 
+    const [text, setText] = useState({
+        line1: ''
+    });
+
+    const textRef = useRef();
+
+    const addLine = (obj, newLine) => {
+        setText(obj);
+    };
 
     useEffect(() => {
-        const addChar = setInterval(() => {
-            setText(text = text + textArray[counter]);
-            setCounter(counter = counter+1);
+        let newLine = '';
+        let counter = 0;
+        const addLine1 = setInterval(() => {
+            newLine = newLine + textArray[counter];
+            addLine({...text, line1: newLine}, newLine);
+            counter++;
             if(counter >= textArray.length){
-                clearInterval(addChar);
+                clearInterval(addLine1);
             }
         }, 50);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return(
         <Layout>
             <SEO title="Home" />
             <div className={indexStyles.container}>
-                <p>{text}</p>
+                <p ref={textRef}>{text.line1}</p>
             </div>
         </Layout>
     )
