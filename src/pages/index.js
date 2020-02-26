f990ea37c7b9001ef4a4dae5739b45bc2f20fd6c
hss-smee <seanmee8186@gmail.com>
2020-02-26 10:12:00 -0800

    finished ugly TW functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,29 +6,94 @@ import SEO from "../components/seo"
 
 const IndexPage = () => {
 
-    const textArray = 'My name is Sean Mee I am a Full Stack Web Developer Living in Southern California Specializing in JavaScript'.split('');
+    const text1Array = 'My name is Sean Mee'.split('');
+    const text2Array = 'I am a Full Stack Web Developer'.split('');
+    const text3Array = 'Living in Southern California'.split('');
+    const text4Array = 'Specializing in JavaScript'.split('');
 
     const [text, setText] = useState({
-        line1: ''
+        line1: '',
+        line2: '',
+        line3: '',
+        line4: ''
     });
 
-    const textRef = useRef();
+    const text1Ref = useRef();
+    const text2Ref = useRef();
+    const text3Ref = useRef();
+    const text4Ref = useRef();
 
     const addLine = (obj, newLine) => {
         setText(obj);
     };
 
-    useEffect(() => {
+    const typeWriter = () => {
         let newLine = '';
         let counter = 0;
+
         const addLine1 = setInterval(() => {
-            newLine = newLine + textArray[counter];
-            addLine({...text, line1: newLine}, newLine);
+            newLine = newLine + text1Array[counter];
+            addLine({
+                line1: newLine
+            }, newLine);
             counter++;
-            if(counter >= textArray.length){
+
+            if(counter >= text1Array.length){
                 clearInterval(addLine1);
+                newLine = '';
+                counter = 0;
+
+                const addLine2 = setInterval(() => {
+                    newLine = newLine + text2Array[counter];
+                    addLine({
+                        line1: text1Ref.current.innerText,
+                        line2: newLine
+                    });
+                    counter++;
+
+                    if(counter >= text2Array.length){
+                        clearInterval(addLine2);
+                        newLine = '';
+                        counter = 0;
+
+                        const addLine3 = setInterval(() => {
+                            newLine = newLine + text3Array[counter];
+                            addLine({
+                                line1: text1Ref.current.innerText,
+                                line2: text2Ref.current.innerText,
+                                line3: newLine
+                            });
+                            counter++;
+
+                            if (counter >= text3Array.length) {
+                                clearInterval(addLine3)
+                                newLine = '';
+                                counter = 0;
+
+                                const addLine4 = setInterval(() => {
+                                    newLine = newLine + text4Array[counter];
+                                    addLine({
+                                        line1: text1Ref.current.innerText,
+                                        line2: text2Ref.current.innerText,
+                                        line3: text3Ref.current.innerText,
+                                        line4: newLine
+                                    });
+                                    counter++;
+
+                                    if (counter >= text4Array.length) {
+                                        clearInterval(addLine4)
+                                    }
+                                }, 50)
+                            }
+                        }, 50)
+                    }
+                }, 50)
             }
         }, 50);
+    };
+
+    useEffect(() => {
+        typeWriter()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
@@ -36,7 +101,10 @@ const IndexPage = () => {
         <Layout>
             <SEO title="Home" />
             <div className={indexStyles.container}>
-                <p ref={textRef}>{text.line1}</p>
+                <p ref={text1Ref}>{text.line1}</p>
+                <p ref={text2Ref}>{text.line2}</p>
+                <p ref={text3Ref}>{text.line3}</p>
+                <p ref={text4Ref}>{text.line4}</p>
             </div>
         </Layout>
     )
