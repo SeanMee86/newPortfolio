ebe6e531c31c120393f352b70a93adbdb2e49f7f
hss-smee <seanmee8186@gmail.com>
2020-02-28 10:09:19 -0800

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import SEO from "../components/seo"
 import TypeWriter from "../assets/scripts/typewriter"
 import profilePic from "../images/sean-mee-min.jpg.webp"
 
-
 const IndexPage = () => {
 
     const linesArray = [
