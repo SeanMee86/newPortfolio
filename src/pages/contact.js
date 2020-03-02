79764aecb81e687ad4f63e6c970b3f342d59d4e4
hss-smee <seanmee8186@gmail.com>
2020-03-02 13:54:51 -0800

    added contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,8 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+import contactStyles from './contact.module.scss';
+
 const Contact = () => {
 
     return (
@@ -10,6 +12,25 @@ const Contact = () => {
             <SEO title={'Contact'}/>
             <div>
                 <h1>Contact</h1>
+            </div>
+            <div>
+                <form name="contact" method="POST" data-netlify="true">
+                    <p>
+                        <label className={contactStyles.labels}><span>Name:</span> <input type="text" name="name" /></label>
+                    </p>
+                    <p>
+                        <label className={contactStyles.labels}><span>Email:</span> <input type="email" name="email" /></label>
+                    </p>
+                    <p>
+                        <label className={contactStyles.labels}><span>Subject:</span> <input type="text" name="subject" /></label>
+                    </p>
+                    <p>
+                        <label className={contactStyles.labels}><span>Message:</span> <textarea name="message" /></label>
+                    </p>
+                    <p>
+                        <button className={contactStyles.contactBtn} type="submit">Send</button>
+                    </p>
+                </form>
             </div>
         </Layout>
     )
