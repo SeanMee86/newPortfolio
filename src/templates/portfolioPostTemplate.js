86cb73f82b0fd1fa26d4d6a791659e83fbc3251e
hss-smee <seanmee8186@gmail.com>
2020-03-03 09:47:35 -0800

    styling updates, continuing build on portfolio post template

--- src/templates/portfolioPostTemplate.js
+++ src/templates/portfolioPostTemplate.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {graphql} from "gatsby";
 import Layout from "../components/layout";
@@ -20,6 +20,7 @@ export const query = graphql`
           }
         }
         portfolioPostImage {
+          title
           file {
             url
           }
@@ -33,14 +34,40 @@ const PortfolioPost = (props) => {
 
     const postData = props.data.contentfulPortfolioPost;
 
-    console.log(postData)
+    const [currentImage, setCurrentImage] = useState({
+        image: postData.portfolioPostImage[0].file.url,
+        alt: postData.portfolioPostImage[0].title
+    });
+
+    const changeImageHandler = (url, altTag) => {
+        setCurrentImage({
+            image: url,
+            alt: altTag
+
+        });
+    };
+
+    /* eslint-disable */
+    const createCarousel = () => postData.portfolioPostImage.map((img, ind) => (
+        <img
+            className={portfolioPostStyles.imgThumbnail}
+            key={ind}
+            onClick={() => changeImageHandler(img.file.url, img.title)}
+            src={img.file.url}
+            alt={img.title}/>
+    ));
+    /* eslint-enable */
+
     return (
         <Layout>
             <SEO title={postData.portfolioPostTitle}/>
             <div>
                 <h1>{postData.portfolioPostTitle}</h1>
-                <img className={portfolioPostStyles.portfolioImg} src={postData.portfolioPostImage[0].file.url} alt=""/>
+                <img className={portfolioPostStyles.portfolioImg} src={currentImage.image} alt={currentImage.alt}/>
                 <p>{postData.portfolioPostSnippet.portfolioPostSnippet}</p>
+                <div className={portfolioPostStyles.carousel}>
+                {createCarousel()}
+                </div>
             </div>
         </Layout>
     )
