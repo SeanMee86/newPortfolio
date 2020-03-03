9aa3bd69731621a6ecdc572577c46a422def6639
SeanMee86 <sean.mee86@yahoo.com>
2020-03-03 06:13:33 -0800

    updating portfolio post template

--- src/templates/portfolioPostTemplate.js
+++ src/templates/portfolioPostTemplate.js
@@ -3,11 +3,15 @@ import React from 'react'
 import {graphql} from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import portfolioPostStyles from './portfolioPost.module.scss'
 
 export const query = graphql`
     query($slug: String!) {
       contentfulPortfolioPost(slug: {eq: $slug}) {
         portfolioPostTitle
+        portfolioPostSnippet {
+            portfolioPostSnippet
+        }
         portfolioPostBody {
           content {
             content {
@@ -29,11 +33,14 @@ const PortfolioPost = (props) => {
 
     const postData = props.data.contentfulPortfolioPost;
 
+    console.log(postData)
     return (
         <Layout>
             <SEO title={postData.portfolioPostTitle}/>
             <div>
                 <h1>{postData.portfolioPostTitle}</h1>
+                <img className={portfolioPostStyles.portfolioImg} src={postData.portfolioPostImage[0].file.url} alt=""/>
+                <p>{postData.portfolioPostSnippet.portfolioPostSnippet}</p>
             </div>
         </Layout>
     )
