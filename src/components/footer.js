dc83343958426ce0a13a22d9d6c8e917ce895332
hss-smee <seanmee8186@gmail.com>
2020-03-09 09:08:35 -0700

    font sizing updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 const Footer = () => {
+    const style = {
+        marginTop: '20px'
+    };
     return (
-        <footer>
+        <footer style={style}>
             Website Created by Sean Mee using GatsbyJS and Contentful CMS &copy; {new Date().getFullYear()}
         </footer>
     )
