001b2aa72890fa186e0d312301875d264d42f299
SeanMee86 <seanmeedev@gmail.com>
2023-01-14 12:57:20 -0800

    Revert "change script to noscript"
    This reverts commit ede27fe629639170f722deddf70c8b6114d16ec3.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,12 +10,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 `
 
 const gtmPixel = `
-<iframe
- src="https://www.googletagmanager.com/ns.html?id=GTM-N5QR68H"
- height="0"
- width="0"
- style="display:none;visibility:hidden">  
-</iframe>
+<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5QR68H"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
 `
 
 export const onRenderBody = ({
@@ -47,7 +43,7 @@ export const onRenderBody = ({
     />
   ]
   const postBodyComponents = [
-    <noscript
+    <script
       key={'gtm-pixel'}
       dangerouslySetInnerHTML={{__html: gtmPixel}}
     />
